5eaeb69c5944c1eb7f2a0fced258e9e32f1f1f34
Nasir Khan Saikat <nasir8891@gmail.com>
2026-09-23 21:54:39 +0600

    removed focus classes

--- resources/views/includes/errors.blade.php
+++ resources/views/includes/errors.blade.php
@@ -19,7 +19,7 @@
         <button
             type="button"
             @click="show = false"
-            class="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+            class="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
             aria-label="Close"
         >
             <svg class="w-3 h-3" fill="none" viewBox="0 0 14 14">
